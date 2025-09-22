eb8fad3003ee4356da8e0a7e638c4421d806e6ab
aryan2310-pixel <thearyanraj0@gmail.com>
2025-09-22 09:58:56 +0530

    debug test

--- Frontend/crowd-care/src/Pages/Login.jsx
+++ Frontend/crowd-care/src/Pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     setError("");
 
     try {
-      const response = await fetch("/api/auth/login", {
+      const response = await fetch("https://crowd-care-r1ub.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ emailOrUsername, password }),
